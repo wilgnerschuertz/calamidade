d6fc636a344ca498c76629ac1fb7935eb88fc558
viniciusddrft <viniciusddrft5@gmail.com>
2024-05-14 17:06:58 -0300

    feat:card-button

--- design_system/lib/design_system.dart
+++ design_system/lib/design_system.dart
@@ -1,4 +1,5 @@
 library design_system;
 
 export 'src/buttons/coo_button/coo_button.dart';
+export 'src/buttons/card_button/card_button.dart';
 export 'src/themes/theme.dart';
---
+++ design_system/lib/src/buttons/card_button/card_button.dart
@@ -0,0 +1,57 @@
+import 'package:design_system/design_system.dart';
+import 'package:flutter/material.dart';
+
+class CardButton extends StatelessWidget {
+  const CardButton({
+    super.key,
+    required this.icon,
+    required this.labelPrimary,
+    required this.labelSecondary,
+    required this.onPressed,
+  });
+
+  final Widget icon;
+  final String labelPrimary, labelSecondary;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          child: Row(
+            children: [
+              icon,
+              Padding(
+                padding: const EdgeInsets.only(left: 8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      labelPrimary,
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: Theme.of(context)
+                              .extension<CoopartilharColorsExtension>()!
+                              .black),
+                    ),
+                    Text(
+                      labelSecondary,
+                      style: TextStyle(
+                          fontSize: 12,
+                          color: Theme.of(context)
+                              .extension<CoopartilharColorsExtension>()!
+                              .grey),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
