683c78bd7b1dd5defb6c440fde6f2a138ee76ed7
Daniel Brandao <daniel.brandao@cubos.io>
2024-05-13 21:14:43 -0300

    feat:(Errors) added default and base expection

---
+++ app/lib/app/core/errors/base_exception.dart
@@ -0,0 +1,10 @@
+abstract class BaseException implements Exception {
+  const BaseException({
+    required this.message,
+
+    this.stackTracing,
+  });
+
+  final String message;
+  final dynamic stackTracing;
+}
