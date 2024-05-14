3c2eedad8195c311b46ccd02b00b99054ba08005
LeBaleiro <lehbaleiro@gmail.com>
2024-05-14 17:27:20 -0300

    fix(env): resolvendo conflitos

--- app/lib/main_prod.dart
+++ app/lib/main_prod.dart
@@ -1,22 +1,6 @@
-import 'package:app/home_page.dart';
+import 'package:app/app/app_widget.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const HomePage(),
-    );
-  }
+  runApp(const AppWidget());
 }
