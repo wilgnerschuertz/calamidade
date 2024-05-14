45cf41d8827030d2711a448d01440b05f7707d80
tabata.santos <tabata.gabriela@hotmail.com>
2024-05-14 16:42:58 -0300

    adicionado botão ao design system
    Co-authored-by: wilgnerschuertz <wilgnerschuertz@users.noreply.github.com>
    Co-authored-by: Daniel <brandaoti@users.noreply.github.com>

--- design_system/lib/src/buttons/coo_button/coo_button.dart
+++ design_system/lib/src/buttons/coo_button/coo_button.dart
@@ -1,10 +1,56 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+
+import '../../../design_system.dart';
 
 class CooButton extends StatelessWidget {
-  const CooButton({super.key});
+  final Color buttonColor;
+  final bool isLoading;
+  final VoidCallback onPressed;
+  final Color textColor;
+  final String text;
+  final bool enable;
+  final Icon? icon;
+  const CooButton({
+    super.key,
+    required this.buttonColor,
+    required this.isLoading,
+    required this.onPressed,
+    required this.textColor,
+    required this.text,
+    required this.enable,
+    this.icon,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final colors = Theme.of(context).extension<CoopartilharColorsExtension>()!;
+
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: enable ? colors.success : colors.grey,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        minimumSize: const Size.fromHeight(50),
+        maximumSize: const Size.fromHeight(50),
+        padding: const EdgeInsets.all(10),
+        elevation: 0,
+      ),
+      onPressed: isLoading || !enable ? null : onPressed,
+      child: isLoading
+          ? const CircularProgressIndicator()
+          : Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  text,
+                  style: TextStyle(
+                    color: textColor,
+                  ),
+                ),
+                icon ?? Container(),
+              ],
+            ),
+    );
   }
 }
