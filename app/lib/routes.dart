595f44dc6289398da781c68f07951079f27242c3
Elio Galdino <eliogaldino79@outlook.com>
2024-05-14 19:34:02 -0300

    change android app icon

--- app/lib/routes.dart
+++ app/lib/routes.dart
@@ -1,28 +1,20 @@
-import 'package:app/app/(public)/ask_help/ask_help_page.dart' as a0;
-import 'package:app/app/(public)/ask_help/new_ask_help/new_ask_help_page.dart' as a1;
-import 'package:app/app/(public)/auth/login/login_page.dart' as a2;
-import 'package:app/app/(public)/auth/register/register_page.dart' as a3;
-import 'package:app/app/(public)/home/home_page.dart' as a4;
-import 'package:app/app/(public)/splash/splash_page.dart' as a5;
-import 'package:core_module/core_module.dart';
+import 'package:routefly/routefly.dart';
+
+import 'app/(public)/ask_help/ask_help_page.dart' as a3;
+import 'app/(public)/ask_help/new_ask_help/new_ask_help_page.dart' as a2;
+import 'app/(public)/auth/login/login_page.dart' as a1;
+import 'app/(public)/auth/register/register_page.dart' as a0;
+import 'app/(public)/home/home_page.dart' as a5;
+import 'app/(public)/splash/splash_page.dart' as a4;
 
 List<RouteEntity> get routes => [
   RouteEntity(
-    key: '/ask_help',
-    uri: Uri.parse('/ask_help'),
+    key: '/auth/register',
+    uri: Uri.parse('/auth/register'),
     routeBuilder: (ctx, settings) => Routefly.defaultRouteBuilder(
       ctx,
       settings,
-      const a0.AskHelpPage(),
-    ),
-  ),
-  RouteEntity(
-    key: '/ask_help/new_ask_help',
-    uri: Uri.parse('/ask_help/new_ask_help'),
-    routeBuilder: (ctx, settings) => Routefly.defaultRouteBuilder(
-      ctx,
-      settings,
-      const a1.NewAskHelpPage(),
+      const a0.RegisterPage(),
     ),
   ),
   RouteEntity(
@@ -31,16 +23,34 @@ List<RouteEntity> get routes => [
     routeBuilder: (ctx, settings) => Routefly.defaultRouteBuilder(
       ctx,
       settings,
-      const a2.LoginPage(),
+      const a1.LoginPage(),
     ),
   ),
   RouteEntity(
-    key: '/auth/register',
-    uri: Uri.parse('/auth/register'),
+    key: '/ask_help/new_ask_help',
+    uri: Uri.parse('/ask_help/new_ask_help'),
     routeBuilder: (ctx, settings) => Routefly.defaultRouteBuilder(
       ctx,
       settings,
-      const a3.RegisterPage(),
+      const a2.NewAskHelpPage(),
+    ),
+  ),
+  RouteEntity(
+    key: '/ask_help',
+    uri: Uri.parse('/ask_help'),
+    routeBuilder: (ctx, settings) => Routefly.defaultRouteBuilder(
+      ctx,
+      settings,
+      const a3.AskHelpPage(),
+    ),
+  ),
+  RouteEntity(
+    key: '/splash',
+    uri: Uri.parse('/splash'),
+    routeBuilder: (ctx, settings) => Routefly.defaultRouteBuilder(
+      ctx,
+      settings,
+      const a4.SplashPage(),
     ),
   ),
   RouteEntity(
@@ -49,31 +59,22 @@ List<RouteEntity> get routes => [
     routeBuilder: (ctx, settings) => Routefly.defaultRouteBuilder(
       ctx,
       settings,
-      const a4.HomePage(),
-    ),
-  ),
-  RouteEntity(
-    key: '/',
-    uri: Uri.parse('/'),
-    routeBuilder: (ctx, settings) => Routefly.defaultRouteBuilder(
-      ctx,
-      settings,
-      const a5.SplashPage(),
+      const a5.HomePage(),
     ),
   ),
 ];
 
 const routePaths = (
   path: '/',
+  auth: (
+    path: '/auth',
+    register: '/auth/register',
+    login: '/auth/login',
+  ),
   askHelp: (
     path: '/ask_help',
     newAskHelp: '/ask_help/new_ask_help',
   ),
-  auth: (
-    path: '/auth',
-    login: '/auth/login',
-    register: '/auth/register',
-  ),
+  splash: '/splash',
   home: '/home',
-  splash: '/',
 );
