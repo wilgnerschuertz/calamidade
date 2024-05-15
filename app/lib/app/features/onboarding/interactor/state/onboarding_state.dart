31b496647d4b87ef1e62d5515646f90b450f5b76
murilinhops <murilinhorps@gmail.com>
2024-05-15 12:45:19 -0300

    fix: Adjust OnboardingErrorState implementation

--- app/lib/app/features/onboarding/interactor/state/onboarding_state.dart
+++ app/lib/app/features/onboarding/interactor/state/onboarding_state.dart
@@ -1,18 +1,21 @@
-import 'package:coopartilhar/app/features/onboarding/interactor/exceptions/onboarding_exception.dart';
+import 'package:coopartilhar/app/features/onboarding/interactor/entities/onboarding_entity.dart';
 import 'package:core_module/core_module.dart';
 
 class OnboardingInitialState extends InitialState {}
 
 class OnboardingLoadingState extends LoadingState {}
 
-class OnboardingSuccsessState extends SuccessState<String> {
-  const OnboardingSuccsessState({
+class OnboardingSuccessState extends SuccessState<OnboardingEntity> {
+  const OnboardingSuccessState({
     required super.data,
   });
 }
 
-class OnboardingErrorState extends ErrorState<OnboardindException> {
-  OnboardingErrorState({
-    required super.exception,
-  });
+class OnboardingErrorState<OnboardingException extends BaseException>
+    extends ErrorState {
+  const OnboardingErrorState(
+    this.onboardingException,
+  ) : super(exception: onboardingException);
+
+  final OnboardingException onboardingException;
 }
