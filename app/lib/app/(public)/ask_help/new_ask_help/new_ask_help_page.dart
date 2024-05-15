0a8791d26390371aef2d4eee4233d470a3362816
Kallew Machado <kallewcarlos@gmail.com>
2024-05-15 14:13:54 -0300

    feat(AskHelp): add input form in new_ask_help_page

--- app/lib/app/(public)/ask_help/new_ask_help/new_ask_help_page.dart
+++ app/lib/app/(public)/ask_help/new_ask_help/new_ask_help_page.dart
@@ -1,23 +1,78 @@
+import 'package:design_system/design_system.dart';
 import 'package:flutter/material.dart';
 
-class NewAskHelpPage extends StatelessWidget {
-  const NewAskHelpPage({super.key});
+class NewAskHelpPage extends StatefulWidget {
+  const NewAskHelpPage({super.key, this.title = 'Solicitação'});
+  final String title;
+
+  @override
+  State<NewAskHelpPage> createState() => _NewAskHelpPageState();
+}
+
+class _NewAskHelpPageState extends State<NewAskHelpPage> {
+  String preApprovedValue = '5.500,00';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('NewAskHelpPage'),
+        title: Text(widget.title),
       ),
-      body: Column(
-        children: [
-          const Placeholder(),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/ask_help');
-              },
-              child: const Text('AskHelpPage')),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text('Valor pré-aprovado'),
+                  Container(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 2, horizontal: 8),
+                    decoration: BoxDecoration(
+                        color: CoopartilharColors.of(context).lightGrey,
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(8))),
+                    child: Text(
+                      'R\$ $preApprovedValue',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: CoopartilharColors.of(context).primary),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              const Text('Titulo da solicitação*'),
+              TextFormField(),
+              const SizedBox(height: 10),
+              const Text('CPF do Assistido*'),
+              TextFormField(),
+              const SizedBox(height: 10),
+              const Text('Chave pix'),
+              TextFormField(),
+              const SizedBox(height: 10),
+              const Text('Banco'),
+              TextFormField(),
+              const SizedBox(height: 10),
+              const Text('Agência'),
+              TextFormField(),
+              const SizedBox(height: 10),
+              const Text('Conta'),
+              TextFormField(),
+              const SizedBox(height: 10),
+              const Text('Valor*'),
+              TextFormField(),
+              const SizedBox(height: 10),
+              const Text('Enviar arquivo'),
+              const SizedBox(height: 10),
+              const Text('Breve descrição'),
+              TextFormField(),
+            ],
+          ),
+        ),
       ),
     );
   }
