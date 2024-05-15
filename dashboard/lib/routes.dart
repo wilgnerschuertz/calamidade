33b03c3eef983873ea12925eb69c1b20a8fbcac0
wilgnerdeveloper <wilgnerdeveloper@gmail.com>
2024-05-15 07:55:23 -0400

    fix: correcao dos imports routes.

--- dashboard/lib/routes.dart
+++ dashboard/lib/routes.dart
@@ -3,16 +3,16 @@ import 'package:routefly/routefly.dart';
 import 'app/(public)/auth/login_page.dart' as a0;
 
 List<RouteEntity> get routes => [
-  RouteEntity(
-    key: '/auth/login',
-    uri: Uri.parse('/auth/login'),
-    routeBuilder: (ctx, settings) => Routefly.defaultRouteBuilder(
-      ctx,
-      settings,
-      const a0.LoginPage(),
-    ),
-  ),
-];
+      RouteEntity(
+        key: '/auth/login',
+        uri: Uri.parse('/auth/login'),
+        routeBuilder: (ctx, settings) => Routefly.defaultRouteBuilder(
+          ctx,
+          settings,
+          const a0.LoginPage(),
+        ),
+      ),
+    ];
 
 const routePaths = (
   path: '/',
