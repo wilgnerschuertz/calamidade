168d1a33e42dd5ffd562aebf35e94ee302482a17
Luiz <lfbaldomarques@gmail.com>
2024-05-14 22:37:44 -0300

    RELEASE: adicionando pacote flutter_svg

--- core_module/lib/core_module.dart
+++ core_module/lib/core_module.dart
@@ -6,6 +6,7 @@ export 'package:auto_injector/auto_injector.dart';
 export 'package:either_dart/either.dart';
 export 'package:routefly/routefly.dart';
 export 'package:uicons/uicons.dart';
+export 'package:flutter_svg/flutter_svg.dart';
 
 export 'src/entities/entity.dart';
 export 'src/errors/errors.dart';
