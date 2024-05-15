42928be811d21dbbd19b0b6297568f4d7db26932
wilgnerdeveloper <wilgnerdeveloper@gmail.com>
2024-05-14 23:22:15 -0400

    fix: correcao tela login
    Correção do botão da tela de login, voltando ao padrão.

--- app/lib/app/(public)/auth/login/login_page.dart
+++ app/lib/app/(public)/auth/login/login_page.dart
@@ -19,30 +19,11 @@ class _LoginPageState extends State<LoginPage> {
         children: [
           const Placeholder(),
           Center(
-            child: Column(
-              children: [
-                CooButton.primary(
-                  icon: Icons
-                      .check, // Só adicionar o Icon, de regra fica no final =))
-                  label: 'Main Button',
-                  onPressed: () {},
-                ),
-                CooButton.secondary(
-                  label: 'Secondary Button',
-                  onPressed: () {},
-                ),
-                CooButton.danger(
-                  icon: Icons
-                      .close, // Só adicionar o Icon, de regra fica no final =))
-                  label: 'Danger Button',
-                  onPressed: () {},
-                ),
-                CooButton.outline(
-                  label: 'Outline Button',
-                  onPressed: () {},
-                ),
-              ],
-            ),
+            child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/auth/register');
+                },
+                child: const Text('ResgisterPage')),
           ),
         ],
       ),
