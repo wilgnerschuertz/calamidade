1c55c9d29c4cc73d008fec5f726ba02bb329d227
Leonardo Kiszewski <kiszewski1999@gmail.com>
2024-05-15 17:22:16 -0300

    feat: create register assisted repository

---
+++ app/lib/app/features/register_assistence/repositories/register_assisted_repository_impl.dart
@@ -0,0 +1,13 @@
+import 'package:coopartilhar/app/features/register_assistence/entities/assisted_entity.dart';
+import 'package:coopartilhar/app/features/register_assistence/repositories/i_register_assisted_repository.dart';
+import 'package:core_module/core_module.dart';
+
+class RegisterAssistedRepositoryImpl extends IRegisterAssistedRepository {
+  @override
+  Future<Output<AssistedEntity>> register({
+    required AssistedEntity assisted,
+  }) {
+    // TODO: implement register
+    throw UnimplementedError();
+  }
+}
