7c2f611eebb5f4bb010951f00b9f0df2ec845c23
viniciusddrft <viniciusddrft5@gmail.com>
2024-05-15 15:56:28 -0300

    fix: correção na entity

--- app/lib/app/features/home/interactor/order_entity.dart
+++ app/lib/app/features/home/interactor/order_entity.dart
@@ -1,13 +1,17 @@
-class OrderEntity {
+import 'package:core_module/core_module.dart';
+
+class OrderEntity extends Entity {
   final double value;
   final String title, helpedName, description, localName;
   final DateTime date;
 
-  const OrderEntity(
+  OrderEntity(
       {required this.value,
       required this.title,
       required this.helpedName,
       required this.description,
       required this.localName,
-      required this.date});
+      required this.date,
+      required String id})
+      : super(id);
 }
