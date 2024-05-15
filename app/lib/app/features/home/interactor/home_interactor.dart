53afbd70a6ee04fa8d2f82adace3f45e9a89bf0e
viniciusddrft <viniciusddrft5@gmail.com>
2024-05-15 17:03:21 -0300

    feat: alteração home

--- app/lib/app/features/home/interactor/home_interactor.dart
+++ app/lib/app/features/home/interactor/home_interactor.dart
@@ -2,13 +2,25 @@ import 'package:coopartilhar/app/features/home/interactor/home_state.dart';
 import 'package:coopartilhar/app/features/home/interactor/interfaces/i_home_repository.dart';
 import 'package:core_module/core_module.dart';
 
+enum CategoryHelp {
+  general('Geral'),
+  houses('Casas'),
+  health('Saude'),
+  animais('Animais'),
+  shelters('Abrigos'),
+  anxiety('Ansiedade');
+
+  const CategoryHelp(this.label);
+  final String label;
+}
+
 class HomeInteractor extends BaseController<BaseState> {
   final IHomeRepository _homeRepository;
   HomeInteractor(this._homeRepository) : super(HomeInitial());
 
-  Future<void> getOrders() async {
+  Future<void> getOrders(CategoryHelp category) async {
     update(HomeLoading());
-    final result = await _homeRepository.getOrders();
+    final result = await _homeRepository.getOrders(category);
     result.fold(
       (left) => update(HomeError(left)),
       (right) => update(HomeSuccess(data: right)),
--- app/lib/app/features/home/interactor/interfaces/i_home_repository.dart
+++ app/lib/app/features/home/interactor/interfaces/i_home_repository.dart
@@ -1,6 +1,7 @@
+import 'package:coopartilhar/app/features/home/interactor/home_interactor.dart';
 import 'package:coopartilhar/app/features/home/interactor/order_entity.dart';
 import 'package:core_module/core_module.dart';
 
 abstract class IHomeRepository {
-  Future<Output<List<OrderEntity>>> getOrders();
+  Future<Output<List<OrderEntity>>> getOrders(CategoryHelp category);
 }
