8f85e6eaae1e35368af43968e4c83f813840d1db
Leonardo Kiszewski <kiszewski1999@gmail.com>
2024-05-15 11:36:16 -0300

    feat: create entity

---
+++ app/lib/app/features/check_affiliated/entities/check_affiliated_entity.dart
@@ -0,0 +1,7 @@
+import 'package:core_module/core_module.dart';
+
+class CheckAffiliatedEntity extends Entity {
+  final bool isAffiliated;
+
+  CheckAffiliatedEntity(super.id, this.isAffiliated);
+}
