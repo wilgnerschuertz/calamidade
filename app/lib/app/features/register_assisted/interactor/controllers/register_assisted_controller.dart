8c69dee73416413731adc818110512458a1ad78e
Leonardo Kiszewski <kiszewski1999@gmail.com>
2024-05-15 22:48:02 -0300

    fix: remove components from UI

--- app/lib/app/features/register_assisted/interactor/controllers/register_assisted_controller.dart
+++ app/lib/app/features/register_assisted/interactor/controllers/register_assisted_controller.dart
@@ -1,7 +1,6 @@
 import 'package:coopartilhar/app/features/register_assisted/entities/assisted_entity.dart';
 import 'package:coopartilhar/app/features/register_assisted/repositories/i_register_assisted_repository.dart';
 import 'package:core_module/core_module.dart';
-import 'package:flutter/widgets.dart';
 
 class RegisterAssistedController extends BaseController {
   final IRegisterAssistedRepository repository;
@@ -9,25 +8,8 @@ class RegisterAssistedController extends BaseController {
   RegisterAssistedController({required this.repository})
       : super(InitialState());
 
-  final emailTextController = TextEditingController();
-  final nameTextController = TextEditingController();
-  final documentTextController = TextEditingController();
-  final phoneTextController = TextEditingController();
-  final latitude = ValueNotifier<double>(0);
-  final longitude = ValueNotifier<double>(0);
-
-  Future<void> register() async {
+  Future<void> register({required AssistedEntity assisted}) async {
     update(LoadingState());
-
-    final assisted = AssistedEntity(
-      '',
-      email: emailTextController.text,
-      name: nameTextController.text,
-      document: documentTextController.text,
-      phone: phoneTextController.text,
-      latitude: latitude.value,
-      longitude: longitude.value,
-    );
 
     final response = await repository.register(assisted: assisted);
 
@@ -41,7 +23,7 @@ class RegisterAssistedController extends BaseController {
       ValidatorsHelper.emailIsValid(text) ? null : 'E-mail inválido';
 
   String? nameValidator(String? text) =>
-      nameTextController.text.isNotEmpty ? null : 'Nome inválido';
+      (text?.isNotEmpty ?? false) ? null : 'Nome inválido';
 
   String? documentValidator(String? text) =>
       ValidatorsHelper.documentIsValid(text) ? null : 'Documento inválido';
