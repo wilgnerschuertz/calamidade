7fe05bdadffab49ea08b31bdf56baa05f4faeb5b
pedroserretti <pedroserretti2@gmail.com>
2024-05-16 10:32:18 -0300

    fix: removendo espaços desnecessários do código e identação

--- app/lib/app/features/ask_help/data/repositories/new_ask_help_repository_impl.dart
+++ app/lib/app/features/ask_help/data/repositories/new_ask_help_repository_impl.dart
@@ -1,4 +1,3 @@
-
 import 'package:coopartilhar/app/features/ask_help/data/adapters/new_ask_help_adapter.dart';
 import 'package:coopartilhar/app/features/ask_help/data/repositories/i_new_ask_help_repository.dart';
 import 'package:coopartilhar/app/features/ask_help/entities/solicitation_entity.dart';
@@ -6,17 +5,21 @@ import 'package:core_module/core_module.dart';
 
 class NewAskHelpRepositoryImpl implements INewAskHelpRepository {
   final IRestClient restClient;
-
-
   NewAskHelpRepositoryImpl(this.restClient);
 
   @override
   Future<Output<void>> saveSolicitation(SolicitationEntity solicitationEntity) async {
     try {
       //TODO: impl correct endpoint
-
       const url = '';
-      final response = await restClient.post<Map<String, dynamic>>(RestClientRequest(path: '$url/${solicitationEntity.id}', data: NewAskHelpAdapter.toJson(solicitationEntity)));
+      final response = await restClient.post<Map<String, dynamic>>(
+        RestClientRequest(
+          path: '$url/${solicitationEntity.id}',
+          data: NewAskHelpAdapter.toJson(
+            solicitationEntity,
+          ),
+        ),
+      );
       if (response.data == null) {
         throw const DefaultException(message: 'Requisição inválida.');
       }
