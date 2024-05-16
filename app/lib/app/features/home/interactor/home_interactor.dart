ec7b1ba3f0a0bae0d757848326682b239ee54e42
Lucas <lucasgeansantos@gmail.com>
2024-05-16 09:32:16 -0300

    fix: fixed name in CategoryHelp

---
+++ app/lib/app/features/home/interactor/home_interactor.dart
@@ -0,0 +1,29 @@
+import 'package:coopartilhar/app/features/home/interactor/home_state.dart';
+import 'package:coopartilhar/app/features/home/interactor/interfaces/i_home_repository.dart';
+import 'package:core_module/core_module.dart';
+
+enum CategoryHelp {
+  general('Geral'),
+  houses('Casas'),
+  health('Saude'),
+  animals('Animais'),
+  shelters('Abrigos'),
+  anxiety('Ansiedade');
+
+  const CategoryHelp(this.label);
+  final String label;
+}
+
+class HomeInteractor extends BaseController<BaseState> {
+  final IHomeRepository _homeRepository;
+  HomeInteractor(this._homeRepository) : super(HomeInitial());
+
+  Future<void> getOrders(CategoryHelp category) async {
+    update(HomeLoading());
+    final result = await _homeRepository.getOrders(category);
+    result.fold(
+      (left) => update(HomeError(left)),
+      (right) => update(HomeSuccess(data: right)),
+    );
+  }
+}
