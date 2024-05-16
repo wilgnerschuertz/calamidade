d07f72d9c384a2d00ac9df1e14a70c3ea0856b92
Nilson Soares <nilsonsoares2011@gmail.com>
2024-05-16 18:58:16 -0300

    remove duplicated import

--- core_module/lib/core_module.dart
+++ core_module/lib/core_module.dart
@@ -20,7 +20,6 @@ export 'src/controllers/controllers.dart';
 export 'src/entities/entity.dart';
 export 'src/errors/errors.dart';
 export 'src/helpers/validators_helper.dart';
-export 'src/helpers/validators_helper.dart';
 export 'src/states/base_state.dart';
 export 'src/types/types.dart';
 export 'src/types/unit.dart';
