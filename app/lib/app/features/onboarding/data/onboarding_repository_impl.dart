6e3dc276eb8ac87d02423272179edadf7c976639
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2024-05-16 10:21:55 -0300

    fix: rename file

--- app/lib/app/features/onboarding/data/onboarding_repository_impl.dart
+++ app/lib/app/features/onboarding/data/onboarding_repository_impl.dart
@@ -1,8 +1,8 @@
 import 'package:coopartilhar/app/features/onboarding/interactor/entities/onboarding_entity.dart';
-import 'package:coopartilhar/app/features/onboarding/interactor/interface/i_onboarding_service_repository.dart';
+import 'package:coopartilhar/app/features/onboarding/interactor/interface/i_onboarding_repository.dart';
 import 'package:core_module/core_module.dart';
 
-class OnboardingServiceRepositoryImpl implements IOnboardingServiceRepository {
+class OnboardingRepositoryImpl implements IOnboardingRepository {
   @override
   Future<Output<OnboardingEntity>> getOnboardingData() {
     // TODO: implement getOnboardingData
