c5efcf7a308a7a8f5d281731540cc1d8f6d97fbf
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2024-05-16 10:52:34 -0300

    feat: add images and change in page structure

--- app/lib/app/(public)/onboarding/onboarding_page.dart
+++ app/lib/app/(public)/onboarding/onboarding_page.dart
@@ -17,79 +17,60 @@ class OnboardingPage extends StatelessWidget {
         .displayMedium
         ?.copyWith(color: colors.textColor, fontSize: 20);
 
-    return Scaffold(
-      body: DecoratedBox(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: NetworkImage(
-                'https://static.vecteezy.com/ti/vetor-gratis/t1/7677104-fundo-branco-com-padrao-geometrico-azul-e-linha-branca-gratis-vetor.jpg'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: CustomScrollView(
-          slivers: [
-            SliverFillRemaining(
-              hasScrollBody: false,
-              child: Stack(
-                alignment: Alignment.center,
+    return Material(
+      child: SafeArea(
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            const Image(image: CooImages.cooBackgroundDetails),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 48),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
                 children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.network(
-                        'https://upload.wikimedia.org/wikipedia/commons/3/3e/Bras%C3%A3o_rio_grande_do_sul.png',
-                        width: 200,
-                        height: 158,
-                        fit: BoxFit.cover,
-                      ),
-                      const SizedBox(
-                        height: 32,
-                      ),
-                      Text('R\$ 1,2 Milhão',
-                          style: Theme.of(context)
-                              .textTheme
-                              .displayLarge
-                              ?.copyWith(
-                                  fontSize: 56,
-                                  color: colors.primary,
-                                  fontWeight: FontWeight.w700)),
-                      Text('arrecadados', style: subTitleStyle),
-                      const SizedBox(
-                        height: 32,
-                      ),
-                      Text('R\$1 Milhão', style: titleStyle),
-                      Text(
-                        'CooPartilhados',
-                        style: subTitleStyle,
-                      ),
-                      const SizedBox(
-                        height: 32,
-                      ),
-                      Text('R\$1.024', style: titleStyle),
-                      Text(
-                        'pessoas assistidas',
-                        style: subTitleStyle,
-                      ),
-                      const SizedBox(
-                        height: 32,
-                      ),
-                      Text('R\$200.024', style: titleStyle),
-                      Text(
-                        'em doações de parceiros',
-                        style: subTitleStyle,
-                      ),
-                    ],
+                  const Spacer(),
+                  const Image(image: CooImages.cooBrand),
+                  const SizedBox(
+                    height: 32,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 48),
-                    child: Align(
-                        alignment: Alignment.bottomCenter,
-                        child: CooButton.primary(
-                          label: 'Começar',
-                          onPressed: () {},
-                          icon: UIcons.regularStraight.arrow_right,
-                        )),
+                  Text('R\$ 1,2 Milhão',
+                      style: Theme.of(context).textTheme.displayLarge?.copyWith(
+                          fontSize: 56,
+                          color: colors.primary,
+                          fontWeight: FontWeight.w700)),
+                  Text('arrecadados', style: subTitleStyle),
+                  const SizedBox(
+                    height: 32,
                   ),
+                  Text('R\$1 Milhão', style: titleStyle),
+                  Text(
+                    'CooPartilhados',
+                    style: subTitleStyle,
+                  ),
+                  const SizedBox(
+                    height: 32,
+                  ),
+                  Text('R\$1.024', style: titleStyle),
+                  Text(
+                    'pessoas assistidas',
+                    style: subTitleStyle,
+                  ),
+                  const SizedBox(
+                    height: 32,
+                  ),
+                  Text('R\$200.024', style: titleStyle),
+                  Text(
+                    'em doações de parceiros',
+                    style: subTitleStyle,
+                  ),
+                  const Spacer(),
+                  Align(
+                      alignment: Alignment.bottomCenter,
+                      child: CooButton.primary(
+                        label: 'Começar',
+                        onPressed: () {},
+                        icon: UIcons.regularStraight.arrow_right,
+                      )),
                 ],
               ),
             ),
