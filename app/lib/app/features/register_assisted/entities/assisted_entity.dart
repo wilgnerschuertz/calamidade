7c04c20a57ffde0605e9c188d3d7d2b32065419d
Leonardo Kiszewski <kiszewski1999@gmail.com>
2024-05-15 22:44:43 -0300

    fix: split properties by line

--- app/lib/app/features/register_assisted/entities/assisted_entity.dart
+++ app/lib/app/features/register_assisted/entities/assisted_entity.dart
@@ -1,8 +1,12 @@
 import 'package:core_module/core_module.dart';
 
 class AssistedEntity extends Entity {
-  final String document, email, name, phone;
-  final double latitude, longitude;
+  final String document;
+  final String email;
+  final String name;
+  final String phone;
+  final double latitude;
+  final double longitude;
 
   AssistedEntity(
     super.id, {
