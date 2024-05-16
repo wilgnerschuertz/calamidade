975120e91237bcc3131d563377d5513bf801074e
PatrickSLuz <patricksantos20101@gmail.com>
2024-05-16 17:08:21 -0300

    feat/#61 - onboarding data/repository

--- app/lib/app/features/onboarding/data/onboarding_repository_impl.dart
+++ app/lib/app/features/onboarding/data/onboarding_repository_impl.dart
@@ -1,11 +1,29 @@
+import 'package:coopartilhar/app/features/onboarding/data/adapter/onboarding_adpter.dart';
 import 'package:coopartilhar/app/features/onboarding/interactor/entities/onboarding_entity.dart';
 import 'package:coopartilhar/app/features/onboarding/interactor/interface/i_onboarding_repository.dart';
 import 'package:core_module/core_module.dart';
 
 class OnboardingRepositoryImpl implements IOnboardingRepository {
+  final IRestClient restClient;
+
+  OnboardingRepositoryImpl(this.restClient);
+
   @override
-  Future<Output<OnboardingEntity>> getOnboardingData() {
-    // TODO: implement getOnboardingData
-    throw UnimplementedError();
+  Future<Output<OnboardingEntity>> getOnboardingData() async {
+    try {
+      // TODO, rever backend endpoint
+      const url = '';
+      final response = await restClient.get(RestClientRequest(path: url));
+
+      if (response.data == null) {
+        throw const DefaultException(message: 'Requisição inválida');
+      }
+
+      return Right(OnboardingAdpter.fromJson(response.data!));
+    } on BaseException catch (err) {
+      return Left(DefaultException(message: err.message));
+    } catch (_) {
+      return const Left(DefaultException(message: 'Erro desconhecido'));
+    }
   }
 }
