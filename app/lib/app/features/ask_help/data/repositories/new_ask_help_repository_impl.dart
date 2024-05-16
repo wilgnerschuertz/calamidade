d9f20e14eb5784bcecb02d04e5fda5908d521997
pedroserretti <pedroserretti2@gmail.com>
2024-05-16 09:56:23 -0300

    feat/53#-impl-repositorio-new_ask_help-correcoes

--- app/lib/app/features/ask_help/data/repositories/new_ask_help_repository_impl.dart
+++ app/lib/app/features/ask_help/data/repositories/new_ask_help_repository_impl.dart
@@ -1,3 +1,5 @@
+
+import 'package:coopartilhar/app/features/ask_help/data/adapters/new_ask_help_adapter.dart';
 import 'package:coopartilhar/app/features/ask_help/data/repositories/i_new_ask_help_repository.dart';
 import 'package:coopartilhar/app/features/ask_help/entities/solicitation_entity.dart';
 import 'package:core_module/core_module.dart';
@@ -5,14 +7,16 @@ import 'package:core_module/core_module.dart';
 class NewAskHelpRepositoryImpl implements INewAskHelpRepository {
   final IRestClient restClient;
 
+
   NewAskHelpRepositoryImpl(this.restClient);
 
   @override
   Future<Output<void>> saveSolicitation(SolicitationEntity solicitationEntity) async {
     try {
       //TODO: impl correct endpoint
+
       const url = '';
-      final response = await restClient.post<Map<String, dynamic>>(RestClientRequest(path: '$url/${solicitationEntity.id}'));
+      final response = await restClient.post<Map<String, dynamic>>(RestClientRequest(path: '$url/${solicitationEntity.id}', data: NewAskHelpAdapter.toJson(solicitationEntity)));
       if (response.data == null) {
         throw const DefaultException(message: 'Requisição inválida.');
       }
