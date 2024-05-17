d2a701e6e595a38b2707f7bb3ebfe09a53ba122a
umpedetiago <tiagopinheirobg@gmail.com>
2024-05-17 20:38:55 -0300

    fix textColor3

--- design_system/lib/src/themes/theme.dart
+++ design_system/lib/src/themes/theme.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-const _defaultInputBorder = OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(8)), borderSide: BorderSide.none);
+const _defaultInputBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(8)),
+    borderSide: BorderSide.none);
 
 ThemeData lightTheme(BuildContext context) {
   return ThemeData(
@@ -29,21 +31,33 @@ ThemeData lightTheme(BuildContext context) {
       error: Color(0xffEB5757),
     ),
     textTheme: TextTheme(
-      displayLarge: GoogleFonts.poppins(fontSize: 24, fontWeight: FontWeight.w700),
-      displayMedium: GoogleFonts.poppins(fontSize: 20, fontWeight: FontWeight.w600),
-      displaySmall: GoogleFonts.poppins(fontSize: 20, fontWeight: FontWeight.w400),
-      headlineLarge: GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.w700),
-      headlineMedium: GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.w600),
-      headlineSmall: GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.w400),
-      titleLarge: GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.w700),
-      titleMedium: GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.w600),
-      titleSmall: GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.w400),
+      displayLarge:
+          GoogleFonts.poppins(fontSize: 24, fontWeight: FontWeight.w700),
+      displayMedium:
+          GoogleFonts.poppins(fontSize: 20, fontWeight: FontWeight.w600),
+      displaySmall:
+          GoogleFonts.poppins(fontSize: 20, fontWeight: FontWeight.w400),
+      headlineLarge:
+          GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.w700),
+      headlineMedium:
+          GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.w600),
+      headlineSmall:
+          GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.w400),
+      titleLarge:
+          GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.w700),
+      titleMedium:
+          GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.w600),
+      titleSmall:
+          GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.w400),
       bodyLarge: GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.w700),
-      bodyMedium: GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.w600),
+      bodyMedium:
+          GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.w600),
       bodySmall: GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.w400),
       labelLarge: GoogleFonts.poppins(fontSize: 1, fontWeight: FontWeight.w700),
-      labelMedium: GoogleFonts.poppins(fontSize: 12, fontWeight: FontWeight.w600),
-      labelSmall: GoogleFonts.poppins(fontSize: 12, fontWeight: FontWeight.w400),
+      labelMedium:
+          GoogleFonts.poppins(fontSize: 12, fontWeight: FontWeight.w600),
+      labelSmall:
+          GoogleFonts.poppins(fontSize: 12, fontWeight: FontWeight.w400),
     ),
     extensions: const [
       CoopartilharColors(
@@ -55,7 +69,7 @@ ThemeData lightTheme(BuildContext context) {
         appBackground: Color(0xff151515),
         textColor: Color(0xff333333),
         textColor2: Color(0xff555555),
-        textColor3: Color(0x767373),
+        textColor3: Color(0xff767373),
         middleGrey: Color(0Xff929296),
         grey: Color(0xffb6c5d1),
         lightGrey: Color(0xffF2F2F9),
@@ -146,6 +160,7 @@ class CoopartilharColors extends ThemeExtension<CoopartilharColors> {
     Color? appBackground,
     Color? textColor,
     Color? textColor2,
+    Color? textColor3,
     Color? middleGrey,
     Color? grey,
     Color? lightGrey,
