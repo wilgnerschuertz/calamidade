fe3dee11f1870ac5fff96b6e7892d3d51b6a81c1
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2024-05-17 12:58:23 -0300

    feat: returning mocked data

--- app/lib/app/features/onboarding/data/onboarding_repository_impl.dart
+++ app/lib/app/features/onboarding/data/onboarding_repository_impl.dart
@@ -1,4 +1,3 @@
-import 'package:coopartilhar/app/features/onboarding/data/adapter/onboarding_adpter.dart';
 import 'package:coopartilhar/app/features/onboarding/interactor/entities/onboarding_entity.dart';
 import 'package:coopartilhar/app/features/onboarding/interactor/interface/i_onboarding_repository.dart';
 import 'package:core_module/core_module.dart';
@@ -12,14 +11,19 @@ class OnboardingRepositoryImpl implements IOnboardingRepository {
   Future<Output<OnboardingEntity>> getOnboardingData() async {
     try {
       // TODO, rever backend endpoint
-      const url = '';
-      final response = await restClient.get(RestClientRequest(path: url));
+      // const url = '';
+      // final response = await restClient.get(RestClientRequest(path: url));
+      // if (response.data == null) {
+      //   return const Left(DefaultException(message: 'Requisição inválida'));
+      // }
 
-      if (response.data == null) {
-        throw const DefaultException(message: 'Requisição inválida');
-      }
-
-      return Right(OnboardingAdpter.fromJson(response.data!));
+      return Right(OnboardingEntity(
+        '1',
+        donationsAmount: 100.0,
+        cooPartilhados: 200.0,
+        assistedPeople: 300.0,
+        partnerDonations: 400.0,
+      ));
     } on BaseException catch (err) {
       return Left(DefaultException(message: err.message));
     } catch (_) {
