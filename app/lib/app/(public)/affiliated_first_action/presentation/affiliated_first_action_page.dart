d94c625afe67016be45565a35d10307381157576
pedroserretti <pedroserretti2@gmail.com>
2024-05-16 23:23:32 -0300

    fix: correção na affiliated first action page

--- app/lib/app/(public)/affiliated_first_action/presentation/affiliated_first_action_page.dart
+++ app/lib/app/(public)/affiliated_first_action/presentation/affiliated_first_action_page.dart
@@ -11,54 +11,58 @@ class AffiliatedFirstActionPage extends StatefulWidget {
 class _AffiliatedFirstActionPageState extends State<AffiliatedFirstActionPage> {
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
+      body: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          const Image(image: CooImages.cooBackgroundDetails),
+          Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Center(child: Image.asset('assets/splash/logo_splash.png')),
-              const SizedBox(
-                height: 24,
-              ),
-              const Text(
-                'Bem-vindo',
-                style: TextStyle(fontSize: 32, fontWeight: FontWeight.w600),
-              ),
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text(
-                  'Vamos prosseguir. O que você deseja fazer?',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.w400),
-                ),
-              ),
-              const SizedBox(
-                height: 24,
-              ),
+              Image.asset('assets/splash/logo_splash.png'),
               Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  CardButton(
-                    icon: Image.asset('assets/pngs/need_help.png'),
-                    labelPrimary: 'Preciso de ajuda',
-                    labelSecondary: 'Estou buscando recursos',
-                    onPressed: () => Navigator.pushNamed(context, '/ask_help'),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    child: Text(
+                      'Bem-vindo',
+                      style: TextStyle(fontSize: 30, color: CoopartilharColors.of(context).textColor),
+                    ),
                   ),
-                  const SizedBox(
-                    height: 24,
-                  ),
-                  CardButton(
-                    icon: Image.asset('assets/pngs/want_to_help.png'),
-                    labelPrimary: 'Quero ajudar!',
-                    labelSecondary: 'Quero apadrinhar uma família',
-                    onPressed: () => Navigator.pushNamed(context, '/want_to_help'),
-                  ),
+                  Padding(padding: const EdgeInsets.symmetric(horizontal: 50), child: Text('Vamos prosseguir. O que você deseja fazer?', textAlign: TextAlign.center, style: textTheme.bodySmall!.copyWith(fontSize: 24, color: CoopartilharColors.of(context).textColor))),
                 ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CardButton(
+                      icon: Image.asset('assets/pngs/need_help.png'),
+                      labelPrimary: 'Preciso de ajuda',
+                      labelSecondary: 'Estou buscando recursos',
+                      onPressed: () => Navigator.pushNamed(context, '/ask_help'),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    CardButton(
+                      icon: Image.asset('assets/pngs/want_to_help.png'),
+                      labelPrimary: 'Quero ajudar!',
+                      labelSecondary: 'Quero apadrinhar uma família',
+                      onPressed: () => Navigator.pushNamed(context, '/want_to_help'),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
-        ),
+        ],
       ),
     );
   }
