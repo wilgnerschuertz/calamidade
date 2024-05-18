1464fc78c29e2c9a7bba77345b295c048e1f4d9e
Diego Castro <omegadiegocastro@gmail.com>
2024-05-18 18:02:06 -0300

    fix(mason): remove help template

--- app/lib/app/features/helps/interactor/exceptions/helps_exception.dart
+++
@@ -1,7 +0,0 @@
-import 'package:core_module/core_module.dart';
-
-class HelpsException extends BaseException {
-  HelpsException({
-    required super.message,
-  });
-}
--- app/lib/app/features/helps/interactor/repositories/i_helps_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:coopartilhar/app/features/helps/interactor/entities/helps_entity.dart';
-import 'package:core_module/core_module.dart';
-
-abstract class IHelpsRepository {
-  Future<Output<HelpsEntity>> fetchData();
-}
