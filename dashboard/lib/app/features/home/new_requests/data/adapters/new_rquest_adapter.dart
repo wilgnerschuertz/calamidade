737bd594e505140a27dd171b5cdcea9b56e6ab43
thKali <kaliniga123@gmail.com>
2024-05-18 16:06:26 -0300

    fix: resolvendo merde (merge + merda)

--- dashboard/lib/app/features/home/new_requests/data/adapters/new_rquest_adapter.dart
+++ dashboard/lib/app/features/home/new_requests/data/adapters/new_rquest_adapter.dart
@@ -4,6 +4,9 @@ class NewRequestAdapter {
   static NewRequestEntity fromJson(Map json) {
     return NewRequestEntity(
       json['id'].toString(),
+      titleDescription: json['description'],
+      date: json['date'],
+      requestedIncome: json['requestedIncome'],
       name: json['name'],
       status: StatusNewRequest.status(json['status']),
       city: json['city'],
