987c34e0c243f257488ecda158b881a77bef101e
Juliana Rousseau <juliana.salafia1@gmail.com>
2024-05-18 14:43:37 -0300

    feat: create new fields for new request entity and enum for status

--- dashboard/lib/app/features/home/new_requests/entities/new_request_entity.dart
+++ dashboard/lib/app/features/home/new_requests/entities/new_request_entity.dart
@@ -1,12 +1,39 @@
 import 'package:core_module/core_module.dart';
 
-enum StatusNewRequest { alta, media, baixa }
+enum StatusNewRequest {
+  // TODO: Adicionar os status corretos e corrigir as cores para cada status
+  // no momento ainda não foram decididos quais serão os status
+  low(label: 'Baixa'),
+  medium(label: 'Média'),
+  high(label: 'Alta'),
+  notDefined(label: 'Não definido');
+
+  final String label;
+
+  const StatusNewRequest({required this.label});
+
+  static StatusNewRequest fromId(int id) {
+    switch (id) {
+      case 1:
+        return StatusNewRequest.low;
+      case 2:
+        return StatusNewRequest.medium;
+      case 3:
+        return StatusNewRequest.high;
+      default:
+        return StatusNewRequest.notDefined;
+    }
+  }
+}
 
 class NewRequestEntity extends Entity {
+  final String titleDescription;
   final String name;
-  final StatusNewRequest status;
   final String city;
   final String phone;
+  final String date;
+  final StatusNewRequest status;
+  final String requestedIncome;
   final String bank;
   final String agency;
   final String account;
@@ -15,10 +42,13 @@ class NewRequestEntity extends Entity {
 
   NewRequestEntity(
     super.id, {
+    required this.titleDescription,
     required this.name,
-    required this.status,
     required this.city,
     required this.phone,
+    required this.date,
+    required this.status,
+    required this.requestedIncome,
     required this.bank,
     required this.agency,
     required this.account,
