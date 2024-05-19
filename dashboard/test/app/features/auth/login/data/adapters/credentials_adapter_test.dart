eaffadc3ef0cbe01b88185b41d143a3e3b1015d2
Lucas Amorim <58087933+lucassamorim@users.noreply.github.com>
2024-05-19 19:44:03 -0300

    feat: adicionar o teste do adapter

---
+++ dashboard/test/app/features/auth/login/data/adapters/credentials_adapter_test.dart
@@ -0,0 +1,12 @@
+import 'package:core_module/core_module.dart';
+import 'package:dashboard/app/features/auth/login/data/adapters/credentials_adapter.dart';
+import 'package:dashboard/app/features/auth/login/interactor/entities/credentials_entity.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('credentials adapter ...', () {
+    final credentialsMap = CredentialsAdapter.toMap(CredentialsEntity(
+        const Uuid().v4(), 'email@email.com.br', 'vinte@#trinta'));
+    expect(credentialsMap, isA<Map<String, dynamic>>());
+  });
+}
