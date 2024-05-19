25d3de1b846f0116b783d7c2cf1d2fdd1f7bd9a4
Lucas Amorim <58087933+lucassamorim@users.noreply.github.com>
2024-05-19 20:26:18 -0300

    fix: injetar as dependências

--- dashboard/lib/injector.dart
+++ dashboard/lib/injector.dart
@@ -1,7 +1,17 @@
 import 'package:core_module/core_module.dart';
+import 'package:dashboard/app/features/auth/login/data/login_repository_impl.dart';
+import 'package:dashboard/app/features/auth/login/data/onboarding_repository_impl.dart';
+import 'package:dashboard/app/features/auth/login/interactor/controllers/login_controller.dart';
+import 'package:dashboard/app/features/auth/login/interactor/controllers/onboarding_controller.dart';
+import 'package:dashboard/app/features/auth/login/interactor/interface/i_login_repository.dart';
+import 'package:dashboard/app/features/auth/login/interactor/interface/i_onboarding_repository.dart';
 
 final injector = AutoInjector(
   on: (injector) {
     injector.addInjector(coreModule);
+    injector.add<ILoginRepository>(LoginRepositoryImpl.new);
+    injector.addSingleton(LoginController.new);
+    injector.add<IOnboardingRepository>(OnboardingRepositoryImpl.new);
+    injector.addSingleton(OnboardingController.new);
   },
 );
