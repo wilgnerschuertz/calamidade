d18a8a0609a2d2f98ebc7683052ae99bc289225c
Guilherme Sanches <guysanches@hotmail.com>
2024-05-21 18:21:02 -0300

    fix:#200-ajustes nas telas de registro e cooperado

--- app/lib/app/(public)/auth/register/register_page.dart
+++ app/lib/app/(public)/auth/register/register_page.dart
@@ -47,7 +47,6 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   void dispose() {
     controller.removeListener(listener);
-    controller.dispose();
     super.dispose();
   }
 
@@ -55,19 +54,16 @@ class _RegisterPageState extends State<RegisterPage> {
   Widget build(BuildContext context) {
     final colors = CoopartilharColors.of(context);
     final textTheme = Theme.of(context).textTheme;
-    final keyboardHeight = MediaQuery.of(context).viewInsets.bottom;
+    final keyboardHeight = MediaQuery.of(context).viewInsets.bottom + 20;
 
     return Scaffold(
       appBar: AppBar(
         centerTitle: false,
-        title: Text(
-          'Concluir cadastro',
-          style: textTheme.displayLarge?.copyWith(color: colors.textColor),
-        ),
+        title: Text('Concluir cadastro',
+            style: textTheme.displayLarge?.copyWith(color: colors.textColor)),
         leading: IconButton(
-          icon: Icon(UIcons.regularStraight.angle_small_left),
-          onPressed: Navigator.of(context).pop,
-        ),
+            icon: Icon(UIcons.regularStraight.angle_small_left),
+            onPressed: Navigator.of(context).pop),
         surfaceTintColor: Colors.transparent,
       ),
       resizeToAvoidBottomInset: false,
@@ -75,89 +71,80 @@ class _RegisterPageState extends State<RegisterPage> {
         return Container(
           decoration: const BoxDecoration(
             image: DecorationImage(
-              image: CooImages.cooBackgroundDetails,
-              fit: BoxFit.contain,
-              alignment: Alignment.bottomCenter,
-            ),
+                image: CooImages.cooBackgroundDetails,
+                fit: BoxFit.contain,
+                alignment: Alignment.bottomCenter),
           ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            child: ValueListenableBuilder(
-              valueListenable: controller,
-              builder: (context, value, child) {
-                if (controller.value is LoadingState) {
-                  return const Center(child: CircularProgressIndicator());
-                }
-                return Column(
-                  children: [
-                    Expanded(
-                      child: SingleChildScrollView(
-                        child: SizedBox(
-                          height: constraints.maxHeight,
-                          child: Form(
-                            key: controller.formKey,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: keyboardHeight > 0
-                                  ? MainAxisAlignment.start
-                                  : MainAxisAlignment.spaceBetween,
-                              children: [
-                                const SizedBox(height: 30),
-                                NonEditableRegisterItem(
-                                  title: 'Nome completo',
-                                  value: user.name,
-                                ),
-                                const SizedBox(height: 30),
-                                NonEditableRegisterItem(
-                                  title: 'CPF/CNPJ',
-                                  value: user.document,
-                                ),
-                                const SizedBox(height: 30),
-                                NonEditableRegisterItem(
-                                  title: 'E-mail',
-                                  value: user.email,
-                                ),
-                                const SizedBox(height: 30),
-                                NonEditableRegisterItem(
-                                  title: 'Telefone',
-                                  value: user.phone,
-                                ),
-                                const SizedBox(height: 30),
-                                RegisterItemField(
-                                  title: 'Senha',
-                                  controller: controller.passwordController,
-                                  hint: 'Insira sua senha',
-                                  isPassword: true,
-                                  validator: controller.passwordValidator,
-                                ),
-                                const SizedBox(height: 30),
-                                RegisterItemField(
-                                  title: 'Confirmar senha',
-                                  controller:
-                                      controller.repeatPasswordController,
-                                  hint: 'Insira novamente sua senha',
-                                  isPassword: true,
-                                  validator: controller.repeatPasswordValidator,
-                                ),
-                                const Spacer(),
-                                CooButton.primary(
-                                  label: 'Entrar',
-                                  onPressed: () => controller.register(user),
-                                ),
-                                const SizedBox(height: 40),
-                              ],
+          child: ValueListenableBuilder(
+            valueListenable: controller,
+            builder: (context, value, child) {
+              if (controller.value is LoadingState) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              return Column(
+                children: [
+                  Expanded(
+                    child: SingleChildScrollView(
+                      padding: const EdgeInsets.only(
+                          bottom: 30, left: 24, right: 24),
+                      child: Form(
+                        key: controller.formKey,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: keyboardHeight > 0
+                              ? MainAxisAlignment.start
+                              : MainAxisAlignment.spaceBetween,
+                          children: [
+                            const SizedBox(height: 30),
+                            NonEditableRegisterItem(
+                              title: 'Nome completo',
+                              value: user.name,
                             ),
-                          ),
+                            const SizedBox(height: 30),
+                            NonEditableRegisterItem(
+                              title: 'CPF/CNPJ',
+                              value: CpfCnpjAdapter.applyMask(user.document),
+                            ),
+                            const SizedBox(height: 30),
+                            NonEditableRegisterItem(
+                              title: 'E-mail',
+                              value: user.email,
+                            ),
+                            const SizedBox(height: 30),
+                            NonEditableRegisterItem(
+                              title: 'Telefone',
+                              value: PhoneAdapter.applyMask(user.phone),
+                            ),
+                            const SizedBox(height: 30),
+                            RegisterItemField(
+                              title: 'Senha',
+                              controller: controller.passwordController,
+                              hint: 'Insira sua senha',
+                              isPassword: true,
+                              validator: controller.passwordValidator,
+                            ),
+                            const SizedBox(height: 30),
+                            RegisterItemField(
+                              title: 'Confirmar senha',
+                              controller: controller.repeatPasswordController,
+                              hint: 'Insira novamente sua senha',
+                              isPassword: true,
+                              validator: controller.repeatPasswordValidator,
+                            ),
+                          ],
                         ),
                       ),
                     ),
-                    SizedBox(
-                      height: keyboardHeight,
-                    )
-                  ],
-                );
-              },
-            ),
+                  ),
+                  CooButton.primary(
+                    label: 'Entrar',
+                    onPressed: () => controller.register(user),
+                  ),
+                  SizedBox(height: keyboardHeight)
+                ],
+              );
+            },
           ),
         );
       }),
