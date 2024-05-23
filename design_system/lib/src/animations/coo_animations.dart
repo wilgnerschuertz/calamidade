70414f6fb9b394bd44a2d122ff39a7ec80820f12
PatrickSLuz <patricksantos20101@gmail.com>
2024-05-23 14:47:12 -0300

    fix/#227 - success animation lottie

--- design_system/lib/src/animations/coo_animations.dart
+++ design_system/lib/src/animations/coo_animations.dart
@@ -1,7 +1,31 @@
+import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
 class CooAnimations {
-  static LottieBuilder cooSuccess = LottieBuilder.asset(
+  static const String _packageName = 'design_system';
+
+  static FutureBuilder _lottieBuilder(AssetLottie assetLottie) {
+    return FutureBuilder<LottieComposition>(
+      future: assetLottie.load(),
+      builder: (_, snapshot) {
+        final composition = snapshot.data;
+
+        if (composition == null) {
+          return const SizedBox.shrink();
+        }
+
+        return Lottie(
+          composition: composition,
+          repeat: false,
+        );
+      },
+    );
+  }
+
+  static final cooSuccess = _lottieBuilder(
+    AssetLottie(
       'assets/lotties/successAnimation.json',
-      repeat: false);
+      package: _packageName,
+    ),
+  );
 }
