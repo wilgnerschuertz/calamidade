4126eca98da6687fbe4c18d0549db7495494a628
back-end <back-end@elshadayitsolutions.com.br>
2024-05-23 12:47:04 -0300

    fix: format

--- app/lib/injector.dart
+++ app/lib/injector.dart
@@ -47,7 +47,8 @@ final injector = AutoInjector(
     injector.add(NewAddressController.new);
     injector.addLazySingleton<IAddressRepository>(AddressRepositoryImpl.new);
     injector.addLazySingleton(AddressController.new);
-    injector.addLazySingleton<IBankAccountRepository>(BankAccountRepositoryImpl.new);
+    injector.addLazySingleton<IBankAccountRepository>(
+        BankAccountRepositoryImpl.new);
     injector.addLazySingleton(BankAccountController.new);
 
     injector.add(HomeController.new);
@@ -61,17 +62,24 @@ final injector = AutoInjector(
 
     injector.addLazySingleton<IAccidentRepository>(AccidentRepositoryImpl.new);
     injector.addLazySingleton<IFileRepository>(FileRepositoryImpl.new);
-    injector.addLazySingleton<INewAskHelpRepository>(NewAskHelpRepositoryImpl.new);
+    injector
+        .addLazySingleton<INewAskHelpRepository>(NewAskHelpRepositoryImpl.new);
     injector.add<AskHelpController>(AskHelpController.new);
-    injector.addLazySingleton<ICheckAffiliatedRepository>(CheckAffiliatedRepositoryImpl.new);
-    injector.addLazySingleton<CheckAffiliatedController>(CheckAffiliatedController.new);
-    injector.addLazySingleton<IRequestDetailsRepository>(RequestDetailsRepositoryImpl.new);
-    injector.addLazySingleton<RequestDetailsController>(RequestDetailsController.new);
+    injector.addLazySingleton<ICheckAffiliatedRepository>(
+        CheckAffiliatedRepositoryImpl.new);
+    injector.addLazySingleton<CheckAffiliatedController>(
+        CheckAffiliatedController.new);
+    injector.addLazySingleton<IRequestDetailsRepository>(
+        RequestDetailsRepositoryImpl.new);
+    injector.addLazySingleton<RequestDetailsController>(
+        RequestDetailsController.new);
 
-    injector.addLazySingleton<IForgotPasswordRepository>(ForgotPasswordRepositoryImpl.new);
+    injector.addLazySingleton<IForgotPasswordRepository>(
+        ForgotPasswordRepositoryImpl.new);
     injector.addLazySingleton(ForgotPasswordController.new);
 
-    injector.addLazySingleton<IResetPasswordRepository>(ResetPasswordRepositoryImpl.new);
+    injector.addLazySingleton<IResetPasswordRepository>(
+        ResetPasswordRepositoryImpl.new);
     injector.addLazySingleton(ResetPasswordController.new);
 
     injector.commit();
