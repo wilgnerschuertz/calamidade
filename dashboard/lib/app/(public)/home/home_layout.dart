3f41f8d98937a50c9e8a724e476243c496591c9f
Alexandre | BrAcInhO <bracinho2@hotmail.com>
2024-05-25 05:57:28 -0300

    fix/#275-Solved-comments

--- dashboard/lib/app/(public)/home/home_layout.dart
+++ dashboard/lib/app/(public)/home/home_layout.dart
@@ -25,100 +25,6 @@ class _HomeLayoutState extends State<HomeLayout> {
             child: RouterOutlet(),
           ),
           _RequestDetailsWidget(),
-          // TODO - Separar em um componente e criar a tela com as informações ao clicar em um card
-          // Expanded(
-          //   flex: 2,
-          //   child: Container(
-          //     color: colors.white,
-          //     child: Padding(
-          //       padding:
-          //           const EdgeInsets.symmetric(horizontal: 20, vertical: 32),
-          //       child: Column(
-          //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          //         children: [
-          //           Row(
-          //             crossAxisAlignment: CrossAxisAlignment.center,
-          //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          //             children: [
-          //               Expanded(
-          //                 child: Image(
-          //                   image: CooImages.cooBanricoop,
-          //                   width: MediaQuery.sizeOf(context).width * 0.05,
-          //                 ),
-          //               ),
-          //               Padding(
-          //                 padding: const EdgeInsets.symmetric(horizontal: 8),
-          //                 child: Icon(
-          //                   CooIcons.plusSmall,
-          //                   color: colors.primary,
-          //                   size: MediaQuery.sizeOf(context).width * 0.01,
-          //                 ),
-          //               ),
-          //               Expanded(
-          //                 child: Image(
-          //                   image: CooImages.cooBrand1,
-          //                   width: MediaQuery.sizeOf(context).width * 0.05,
-          //                 ),
-          //               ),
-          //             ],
-          //           ),
-          //           Column(
-          //             children: [
-          //               Text(
-          //                 'R\$ 1,2 Milhão',
-          //                 style: texts.headlineLarge?.copyWith(
-          //                   color: colors.primary,
-          //                   fontSize: 32,
-          //                 ),
-          //               ),
-          //               Text(
-          //                 'arrecadados',
-          //                 style: texts.bodySmall,
-          //               ),
-          //               const SizedBox(height: 16),
-          //               Text(
-          //                 'R\$ 1 Milhão',
-          //                 style: texts.headlineLarge?.copyWith(
-          //                   color: colors.primary,
-          //                 ),
-          //               ),
-          //               Text(
-          //                 'CooPartilhados',
-          //                 style: texts.bodySmall,
-          //               ),
-          //               const SizedBox(height: 16),
-          //               Text(
-          //                 '1.024',
-          //                 style: texts.headlineLarge?.copyWith(
-          //                   color: colors.primary,
-          //                 ),
-          //               ),
-          //               Text(
-          //                 'pessoas assistidas',
-          //                 style: texts.bodySmall,
-          //               ),
-          //               const SizedBox(height: 16),
-          //               Text(
-          //                 'R\$ 200.024',
-          //                 style: texts.headlineLarge?.copyWith(
-          //                   color: colors.primary,
-          //                 ),
-          //               ),
-          //               Text(
-          //                 'em doações de parceiros',
-          //                 style: texts.bodySmall,
-          //               ),
-          //             ],
-          //           ),
-          //           Image(
-          //             image: CooImages.cooFlutterando1,
-          //             width: MediaQuery.sizeOf(context).width * 0.4,
-          //           ),
-          //         ],
-          //       ),
-          //     ),
-          //   ),
-          // ),
         ],
       ),
     );
@@ -414,6 +320,109 @@ class InfoDetailRow extends StatelessWidget {
           ),
         ),
       ],
+    );
+  }
+}
+
+class ResumeCoopartilhar extends StatelessWidget {
+  const ResumeCoopartilhar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final texts = Theme.of(context).textTheme;
+    final colors = CoopartilharColors.of(context);
+
+    return Expanded(
+      flex: 2,
+      child: Container(
+        color: colors.white,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 32),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Image(
+                      image: CooImages.cooBanricoop,
+                      width: MediaQuery.sizeOf(context).width * 0.05,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    child: Icon(
+                      CooIcons.plusSmall,
+                      color: colors.primary,
+                      size: MediaQuery.sizeOf(context).width * 0.01,
+                    ),
+                  ),
+                  Expanded(
+                    child: Image(
+                      image: CooImages.cooBrand1,
+                      width: MediaQuery.sizeOf(context).width * 0.05,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Text(
+                    'R\$ 1,2 Milhão',
+                    style: texts.headlineLarge?.copyWith(
+                      color: colors.primary,
+                      fontSize: 32,
+                    ),
+                  ),
+                  Text(
+                    'arrecadados',
+                    style: texts.bodySmall,
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    'R\$ 1 Milhão',
+                    style: texts.headlineLarge?.copyWith(
+                      color: colors.primary,
+                    ),
+                  ),
+                  Text(
+                    'CooPartilhados',
+                    style: texts.bodySmall,
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    '1.024',
+                    style: texts.headlineLarge?.copyWith(
+                      color: colors.primary,
+                    ),
+                  ),
+                  Text(
+                    'pessoas assistidas',
+                    style: texts.bodySmall,
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    'R\$ 200.024',
+                    style: texts.headlineLarge?.copyWith(
+                      color: colors.primary,
+                    ),
+                  ),
+                  Text(
+                    'em doações de parceiros',
+                    style: texts.bodySmall,
+                  ),
+                ],
+              ),
+              Image(
+                image: CooImages.cooFlutterando1,
+                width: MediaQuery.sizeOf(context).width * 0.4,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
